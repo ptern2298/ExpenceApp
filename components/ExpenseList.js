86d25946cda630b962c9d4e14b3c3c4592208e84
ptern2298 <ptern2298@outlook.com>
2022-11-30 09:14:44 -0800

    - ExpenseList - TotalExpense

---
+++ components/ExpenseList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SafeAreaView, FlatList, StyleSheet } from 'react-native';
+import ExpenseItem from './ExpenseItem';
+
+export default function ExpenseList(props) {
+
+    return(
+        <SafeAreaView>
+            <FlatList data={props.expenses} renderItem={({item}) =>
+                    <ExpenseItem price={item.price}
+                                 title={item.title}
+                                 deleteExpense={props.deleteExpense}
+                                 id={item.id} image={item.image}
+                                 expenseDate={item.expenseDate}
+                    />}
+                keyExtractor={(item) => item.id}
+                extraData={props.deleteExpense}
+            />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    flatListContainer: {
+        backgroundColor: '#FFF',
+        borderRadius: 16,
+        margin: 5,
+        marginBottom: 10,
+        borderTopWidth:1,
+
+    },
+    shadowProp:{
+        shadowColor: '#171717',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    }
+
+})
---
+++ components/TotalExpense.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function TotalExpense(props) {
+
+    const sum = (props.expenses).map(item => parseFloat(item.price)).reduce((prev, next) => prev + next, 0)
+
+    return(
+        <View style={styles.totalItem}>
+            <Text>Total Expense:</Text>
+            <Text style={styles.totalItemTxt}>${sum}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    totalItem: {
+        backgroundColor: '#FFF',
+        padding: 64,
+        borderRadius: 16,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+        marginTop: 20
+    },
+    totalItemTxt: {
+        fontFamily: 'Verdana',
+        fontSize: 32
+    }
+
+})
